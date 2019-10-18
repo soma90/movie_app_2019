97b9ea6fb361b1829153458e5d0e5a4fbdf40b17
kyutae <soma90-1@hanmail.net>
2019-10-18 12:11:36 +0900

    Updates

---
+++ precache-manifest.8df98ab4c7b6a59f046480d521a3d465.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a33459fa0acb3661fbc0ac34c42ad8",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "354323563d8c78da712b",
+    "url": "/movie_app_2019/static/css/main.479b8a3a.chunk.css"
+  },
+  {
+    "revision": "8b85159f93e6d17feac2",
+    "url": "/movie_app_2019/static/js/2.56c38d44.chunk.js"
+  },
+  {
+    "revision": "354323563d8c78da712b",
+    "url": "/movie_app_2019/static/js/main.1f584d8e.chunk.js"
+  },
+  {
+    "revision": "44901d82ce6105999ea4",
+    "url": "/movie_app_2019/static/js/runtime-main.0f07e758.js"
+  }
+]);
